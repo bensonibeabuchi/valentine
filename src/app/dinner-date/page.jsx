c873e261a5426dd2be26fce00ad2e16df7fa9ce1
bensonibeabuchi <ibeabuchibenson@gmail.com>
2024-02-13 15:36:52 +0100

    updated images

--- src/app/dinner-date/page.jsx
+++ src/app/dinner-date/page.jsx
@@ -3,13 +3,7 @@
 
 import React, { useState, useEffect } from 'react';
 import Image from "next/image";
-// import first from "/images/1.jpg"
-// import second from "/public/images/2.jpg"
-// import third from "/public/images/3.jpg"
-// import fourth from "/public/images/4.jpg"
-// import fifth from "/public/images/5.jpg"
-
-import { Navigation, Pagination, Autoplay, Scrollbar, A11y } from 'swiper/modules';
+import { Pagination, Autoplay, Scrollbar, A11y } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -58,27 +52,20 @@ const DinnerDatePage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen mx-auto bg-background2 bg-cover">
       <Swiper className='mx-auto w-4/5'
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      modules={[ Pagination, Scrollbar, A11y, Autoplay]}
       spaceBetween={50}
       slidesPerView={1}
-      // navigation
       pagination={{ clickable: true }}
       autoplay
-      // scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      {/* <SwiperSlide><Image className='mx-auto justify-center items-center grid' src={first} width={400} height={400} alt="images" /></SwiperSlide> */}
-      {/* <SwiperSlide><Image className='mx-auto justify-center items-center grid' src={second} width={400} height={400} alt="images" /></SwiperSlide>
-      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src={third} width={400} height={400} alt="images" /></SwiperSlide>
-      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src={fourth} width={400} height={400} alt="images" /></SwiperSlide>
-      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src={fifth} width={400} height={400} alt="images" /></SwiperSlide> */}
-      <SwiperSlide><img className='mx-auto justify-center items-center grid' width={400} height={400} src="/images/1.jpg" alt="Your Image" /></SwiperSlide>
-      <SwiperSlide><img className='mx-auto justify-center items-center grid' width={400} height={400} src="/images/2.jpg" alt="Your Image" /></SwiperSlide>
-      <SwiperSlide><img className='mx-auto justify-center items-center grid' width={400} height={400} src="/images/3.jpg" alt="Your Image" /></SwiperSlide>
-      <SwiperSlide><img className='mx-auto justify-center items-center grid' width={400} height={400} src="/images/4.jpg" alt="Your Image" /></SwiperSlide>
-      <SwiperSlide><img className='mx-auto justify-center items-center grid' width={400} height={400} src="/images/5.jpg" alt="Your Image" /></SwiperSlide>
 
+      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src="/images/1.png" width={400} height={400} alt="images" /></SwiperSlide>
+      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src="/images/2.png" width={400} height={400} alt="images" /></SwiperSlide>
+      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src="/images/3.png" width={400} height={400} alt="images" /></SwiperSlide>
+      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src="/images/4.jpg" width={400} height={400} alt="images" /></SwiperSlide>
+      <SwiperSlide><Image className='mx-auto justify-center items-center grid' src="/images/5.png" width={400} height={400} alt="images" /></SwiperSlide>
     </Swiper>
     <div className='mt-4 flex flex-col items-center justify-center text-center'>
       <p className="text-xl font-bold">Dinner Date Invitation</p>
